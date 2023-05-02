9872fd3073aae5af39632cf37487893fdb2232e1
joaoalberis <alberisjoao@hotmail.comm>
2023-05-02 18:49:08 -0300

    refatoração do javascript

--- assets/main.js
+++ assets/main.js
@@ -8,32 +8,36 @@ const p = document.querySelector('#modal-content p');
 const fecharModel = document.querySelector('#modal-content span');
 const showMore = document.getElementById('showMore');
 const apiKey = '';
+const fragment = document.createDocumentFragment();
 var page = 1;
-var url;
 var busca;
 
-button.addEventListener('click', function() {
+async function callApi(url){
+    let resultado;
+    await fetch(url).then((response) => response.json())
+        .then(async (result) => {
+            const promise = result.results.map(item => intoFilmes(item, fragment));
+            await Promise.all(promise).then(() => {
+                filmes.append(fragment);
+                resultado = result;
+            })
+        })
+    return resultado;
+}
+
+button.addEventListener('click', async function() {
     busca = document.getElementById('filme').value.replace(/\s+/g, '+');
     if (!/^\++$/.test(busca) && busca != ""){
         document.querySelector('section span').innerHTML = ""
         page = 1;
-        url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${busca}&language=pt-BR&page=${page}`
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.send();
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4){
-                filmes.innerHTML = ""
-                showMore.style.display = 'none'
-                const result = JSON.parse(xhr.responseText);
-                result.results.forEach(getId);
-                if (result.total_results <= 0) document.querySelector('section span').innerHTML = "Não encontramos nenhum filme com esse nome";
-                if (result.total_pages > 1){
-                    showMore.style.display = 'block';
-                    page++;
-                }
-            }
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${busca}&language=pt-BR&page=${page}`;
+        filmes.innerHTML = ""
+        showMore.style.display = 'none'
+        const result = await callApi(url);
+        if (result.total_results <= 0) document.querySelector('section span').innerHTML = "Não encontramos nenhum filme com esse nome";
+        if (result.total_pages > 1){
+            showMore.style.display = 'block';
+            page++;
         }
     }else{
         filmes.innerHTML = ""
@@ -42,63 +46,43 @@ button.addEventListener('click', function() {
     }
 });
 
-showMore.addEventListener('click', function () {
-    url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${busca}&language=pt-BR&page=${page}`
-    fetch(url).then((response) => response.json())
-        .then((result) => {
-            result.results.forEach(getId);
-            if (page >= result.total_pages ) showMore.style.display = 'none';
-            page++
-        })
-        .catch((error) => console.log(error));
+showMore.addEventListener('click', async function () {
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${busca}&language=pt-BR&page=${page}`
+    const result = await callApi(url)
+    if (page >= result.total_pages ) showMore.style.display = 'none';
+    page++
 })
 
-function getId(response){
-    const url = `https://api.themoviedb.org/3/movie/${response.id}?api_key=${apiKey}&language=pt-BR&append_to_response=videos`
-    fetch(url).then((response) => response.json())
-        .then((result) => setHtmlInfoFilmes(result));
+function createP(text){
+    const p = document.createElement('p');
+    p.textContent = text;
+    return p;
 }
 
-function setHtmlInfoFilmes(response){
-    const titulo = response.title;
-    const sinopse = response.overview;
-    const imagem = response.poster_path == null ? "./assets/imagemIndisponivel.png" : `https://image.tmdb.org/t/p/w400${response.poster_path}` ;
-    const lancamento = response.release_date;
-    const classificacao = response.vote_average.toFixed(1);
-    const votos = response.vote_count; 
-    const duracao = response.runtime;
-    const generos = response.genres.map(value => value.name).join(', ');
-    const companhias = response.production_companies.map(value => value.name).join(', ');
-    const video = response.videos.results;
-
-    if (video.length != 0){
-        filmes.innerHTML += `<li class="card">
-            <h3>${titulo}</h3>
-            <img src="${imagem}" alt="post do filme ${titulo}">
-            <p>Sinopse: ${sinopse}</p>
-            <p>Lançamento: ${lancamento}</p>
-            <p>Classificação: ${classificacao}</p>
-            <p>Votos: ${votos}</p>
-            <p>Duração: ${duracao}</p>
-            <p>Generos: ${generos}</p>
-            <p>Companhias: ${companhias}</p>
-            <button onclick="trailerFilme('${video[0].key}')" type="button">Trailer</button>
-        </li>`
-    }else {
-        filmes.innerHTML += `<li class="card">
-            <h3>${titulo}</h3>
-            <img src="${imagem}" alt="post do filme ${titulo}">
-            <p>Sinopse: ${sinopse}</p>
-            <p>Lançamento: ${lancamento}</p>
-            <p>Classificação: ${classificacao}</p>
-            <p>Votos: ${votos}</p>
-            <p>Duração: ${duracao}</p>
-            <p>Generos: ${generos}</p>
-            <p>Companhias: ${companhias}</p>
-            <button onclick="trailerFilme()" type="button">Trailer</button>
-        </li>`
-    }
-    
+function intoFilmes(response, fragment){
+    const url = `https://api.themoviedb.org/3/movie/${response.id}?api_key=${apiKey}&language=pt-BR&append_to_response=videos`
+    return fetch(url).then((response) => response.json())
+        .then((response) => {
+            const imagem = response.poster_path == null ? "./assets/imagemIndisponivel.png" : `https://image.tmdb.org/t/p/w400${response.poster_path}`;
+            const generos = response.genres.map(value => value.name).join(', ');
+            const companhias = response.production_companies.map(value => value.name).join(', ');
+            const { title, overview, release_date, vote_average, vote_count, runtime, videos: { results: video } } = response;
+        
+            const li = document.createElement('li');
+            const h3 = document.createElement('h3');
+            const img = document.createElement('img');
+            const p = document.createElement('p');
+            const button = document.createElement('button');
+            li.classList.add('card');
+            h3.textContent = `${title}`;
+            img.setAttribute('src', `${imagem}`);
+            img.setAttribute('alt', `post do filme ${title}`);
+            video.length != 0 ? button.setAttribute('onclick', `trailerFilme('${video[0].key}')`) : button.setAttribute('onclick', 'trailerFilme()');
+            button.textContent = `Trailer`;
+            li.append(h3, img, createP(`Sinopse: ${overview}`), createP(`Lançamento: ${release_date}`), createP(`Classificação: ${vote_average}`),
+                createP(`Votos: ${vote_count}`), createP(`Duração: ${runtime}`), createP(`Generos: ${generos}`), createP(`Companhias: ${companhias}`), button);
+            fragment.append(li);
+        })
 }
 
 function trailerFilme(key = null){
@@ -109,7 +93,6 @@ function trailerFilme(key = null){
         p.textContent  = 'OBS: Esse filme não possui um trailer no banco de dados!'
         modal.style.display = 'flex'
     }
-    
 }
 
 modal.addEventListener('click', () => {
@@ -121,6 +104,5 @@ modal.addEventListener('click', () => {
 fecharModel.addEventListener('click', () => {
     iframe.src = '';
     modal.style.display = 'none';
-    p.textContent = '';
-    
+    p.textContent = ''; 
 })
